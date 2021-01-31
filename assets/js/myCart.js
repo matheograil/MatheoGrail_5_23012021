6a2b079c7801e8584b4f1e9297b738a7cd238bb0
Mathéo Grail <grail.matheo@protonmail.com>
2021-01-31 12:55:50 +0100

    Ajout du nombre de produit de même type dans le panier

--- assets/js/myCart.js
+++ assets/js/myCart.js
@@ -9,6 +9,6 @@ try {
 	for (data in myCart) {
 		//DOM.
 		let priceWithoutCents = myCart[data]['price']/100;
-		document.querySelector('.myCart__items').insertAdjacentHTML('beforeend', `<div class="myCart__item"><img class="myCart__img" src="${myCart[data]['imageUrl']}"><div class="myCart__description"><p>${myCart[data]['name']}</p><p class="myCart__price">${priceWithoutCents}€</p></div></div>`);
+		document.querySelector('.myCart__items').insertAdjacentHTML('beforeend', `<div class="myCart__item"><img class="myCart__img" src="${myCart[data]['imageUrl']}"><div class="myCart__description"><p><code>${myCart[data]['amount']}x</code> ${myCart[data]['name']}</p><p class="myCart__price">${priceWithoutCents}€</p></div></div>`);
 	}
 }
